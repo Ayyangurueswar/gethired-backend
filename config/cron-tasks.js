fd82665e61218852741ae62f9ab96bccd5f6b13b
Ayyangurueswar <ayyan16.gbps@gmail.com>
2024-06-20 16:37:04 +0530

    changes

--- config/cron-tasks.js
+++ config/cron-tasks.js
@@ -11,14 +11,4 @@ module.exports = {
             rule: "0 0 0 * * ?",
         }
     },
-    myJob: {
-        task: async () => {
-            const events = await fetch('https://gethired-backend.onrender.com/api/jobs')
-            const res = await events.json();
-            return events;
-        },
-        options: {
-            rule: "*/13 * * * *",
-        }
-    }
 }
